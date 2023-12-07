d272fae87e9e065c5922ccfe5c1e3286515c11ca
maximeVa <maxime.vanachter@student.vinci.be>
2023-12-08 00:13:33 +0100

    try to improve css

--- src/app/features/deliveryPage/deliveryPage.module.ts
+++ src/app/features/deliveryPage/deliveryPage.module.ts
@@ -5,9 +5,6 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SharedModule} from "../../shared/shared.module";
 import {RouterOutlet} from '@angular/router';
 import {DeliveryRoundsPageComponent} from "./views/delivery-rounds-page/delivery-rounds-page.component";
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -19,9 +16,6 @@ import { MatListModule } from '@angular/material/list';
     FormsModule,
     ReactiveFormsModule,
     RouterOutlet,
-    MatButtonModule,
-    MatCardModule,
-    MatListModule,
   ]
 })
 export class DeliveryPageModule {
